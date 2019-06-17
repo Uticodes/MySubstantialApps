88949855abbe26dee55cf1a01e493481dc90dfb5
Yublue <etim.utibe@gmail.com>
2019-06-17 20:06:18 +0100

    created LoginActivity

--- app/src/main/java/com/example/mysubstantialapps/LoginActivity.kt
+++ app/src/main/java/com/example/mysubstantialapps/LoginActivity.kt
@@ -1,12 +1,11 @@
 package com.example.mysubstantialapps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class LoginActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
